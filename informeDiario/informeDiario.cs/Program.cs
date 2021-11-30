12f18ac7407884265d420545e952917ee746ea6f
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-11-30 07:07:09 -0800

    informe diario

---
+++ informeDiario/informeDiario.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace informeDiario.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            //This a simple program
+            Console.WriteLine("Hola, cual es tu nombre?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("your name is: " + yourName);
+            Console.ReadLine();
+
+            Console.WriteLine("cual es tu numero favorito?");
+            string FavoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(FavoriteNumber);
+            int total = favNum + 5;
+            Console.WriteLine("tu numero favorito mas 5 is: " + total);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
