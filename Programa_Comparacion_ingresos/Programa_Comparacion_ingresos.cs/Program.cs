e74f1eb6e1c954c3b6ce327cbaa533339c8e8859
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-11-30 15:18:46 -0800

    mas eficiente programa de informe diario

--- Programa_Comparacion_ingresos/Programa_Comparacion_ingresos.cs/Program.cs
+++ Programa_Comparacion_ingresos/Programa_Comparacion_ingresos.cs/Program.cs
@@ -11,25 +11,25 @@ namespace Programa_Comparacion_ingresos.cs
             Console.WriteLine("Hola primera persona, cual es tu nombre?"); //toma el valor de la cadena (nombre)
             string pna1 = Console.ReadLine(); // guarda lo que escribe como pna1
             Console.WriteLine("Cuanto es tu ingreso por hora?");
-            string ingresoXhr1 = Console.ReadLine(); //lee el valor
-            double iXh1 = Convert.ToInt32(ingresoXhr1); //el valor anterior lo convierte en entero
+            double ingresoXhr1 = Convert.ToDouble(Console.ReadLine()); //lee el valor
+            // double iXh1 = Convert.ToInt32(ingresoXhr1); //el valor anterior lo convierte en entero
             Console.WriteLine("Cuantas horas trabajas por semana?");
-            string hsXsem1 = Console.ReadLine();
-            double hXs1 = Convert.ToInt32(hsXsem1); //el valor anterior lo convierte en entero
+            double hsXsem1 = Convert.ToDouble(Console.ReadLine());
+            // double hXs1 = Convert.ToInt32(hsXsem1); //el valor anterior lo convierte en entero
 
             Console.WriteLine("Hola segunda persona, cual es tu nombre?");
             string pna2 = Console.ReadLine(); // guarda lo que escribe como pna1
             Console.WriteLine("Cuanto es tu ingreso por hora?");
-            string ingresoXhr2 = Console.ReadLine(); //lee el valor
-            int iXh2 = Convert.ToInt32(ingresoXhr2); //el valor anterior lo convierte en entero
+            double ingresoXhr2 = Convert.ToDouble(Console.ReadLine()); //lee el valor
+            // int iXh2 = Convert.ToInt32(ingresoXhr2); //el valor anterior lo convierte en entero
             Console.WriteLine("Cuantas horas trabajas por semana?");
-            string hsXsem2 = Console.ReadLine();
-            int hXs2 = Convert.ToInt32(hsXsem2); //el valor anterior lo convierte en entero
+            int hsXsem2 = Convert.ToInt32(Console.ReadLine());
+            // int hXs2 = Convert.ToInt32(hsXsem2); //el valor anterior lo convierte en entero
 
 
             //toma los valores recibidos y hace la multiplicacion de sueldo por hora por el numero de horas por semana por el numero de semanas del ano y lo guarda en la variable total1 y total 2 respectivamente
-            double total1 = iXh1 * hXs1 * 52.143; 
-            double total2 = iXh2 * hXs2 * 52.143;
+            double total1 = ingresoXhr1 * hsXsem1 * 52.143; 
+            double total2 = ingresoXhr2 * hsXsem2 * 52.143;
 
             Console.WriteLine("El salario anual de " + pna1 + " es = " + total1); //escribe el resultado de cada persona
             Console.WriteLine("El salario anual de " + pna2 + " es = " + total2);
