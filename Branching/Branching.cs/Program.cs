0a937c093f74f145502c8eb5a816333f9fbb1ba6
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-11-30 17:51:31 -0800

    correccion de program aprobacion y nuevo programa de Package Express

---
+++ Branching/Branching.cs/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("cual es tu numero favorito");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "you have an awesome fav number." : "you dont have an awesome fav numbes";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            /* int roomTemperature = 80;
+
+            Console.WriteLine("Hola, cual es tu nombre");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hola " + name + ",  cual es la temperatura donde estas?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("its excatly room temperature.");
+            } else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("it is warmer than room temp");
+            } else if (roomTemperature > currentTemperature)
+            {
+                Console.WriteLine("Room temperature is warmer than current temperature");
+            }
+            else
+            {
+                Console.WriteLine("it isnt exactly room temperature.");
+            }
+            Console.ReadLine(); */
+
+
+            /* int currentTemperature = 80;
+            int roomTemperature = 70;
+
+            string comparisonResult = currentTemperature == roomTemperature ? "it is room temp" : "it is not room temp";
+
+            Console.WriteLine(comparisonResult);
+            Console.ReadLine();
+
+             if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("its exactly room temperature.");
+            } else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("its warmer than room temperature");
+            } else if (roomTemperature > currentTemperature)
+            {
+                Console.WriteLine("Room temperature is warmer than current temperature");
+            } else
+            {
+                Console.WriteLine("it isnt exactly room temperature.");
+            } */
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ PackageExpress/PackageExpress.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpress.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio Package Express
+            Console.WriteLine("The Tech Academy.\n Package Express S.A. de C.V.");
+            Console.WriteLine("Hola, Bienvenido a Package Express, por favor siga las instrucciones");
+            Console.WriteLine("Cual es el peso de tu paquete?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight >= 50)
+            {
+                Console.WriteLine("ERROR!!!, \n Paquete demasiado pesado para ser enviado a travez de Package Express; que tenga un buen dia. ");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Cual es el ancho de tu paquete?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cual es la altura de tu paquete?");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cual es la longitud de tu paquete?");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            double dimension = width + height + length;
+            if (dimension >= 50)
+            {
+                Console.WriteLine("ERROR!!!, \n Paquete demasiado grande para enviarse a travez de Package Express; que tenga un buen dia. ");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            double dimension2 = width * height * length;
+            double cotizacion = dimension2 * weight / 100.0;
+            Console.WriteLine("Su total estimado para enviar este paquete es: $" + cotizacion + " Gracias!");
+            Console.ReadLine();
+
+        }
+    }
+}
