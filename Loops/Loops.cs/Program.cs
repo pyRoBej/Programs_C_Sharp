32289c88b21a5b0c66e3217ed6a266ca00f13c14
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-01 10:26:50 -0800

    creación de nuevo loop

--- Loops/Loops.cs/Program.cs
+++ Loops/Loops.cs/Program.cs
@@ -12,18 +12,37 @@ namespace Loops.cs
         {
             Console.WriteLine("piensa un numero entre el 1 y el 10");
             int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 10;
 
-            switch (number)
+            do
             {
-                case 1:
-                    Console.WriteLine("pensaste en el 1, prueba de nuevo");
-                    Console.ReadLine();
-                    break;
-                case 2:
-                    Console.WriteLine("pensaste en el 2, prueba de nuevo");
-                    Console.ReadLine();
-                    break;
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("pensaste en el 1, prueba de nuevo");
+                        Console.WriteLine("piensa un numero entre el 1 y el 10");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("pensaste en el 2, prueba de nuevo");
+                        Console.WriteLine("piensa un numero entre el 1 y el 10");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    
+                    case 10:
+                        Console.WriteLine("pensaste en el 10, es el correcto!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Estas mal.");
+                        Console.WriteLine("piensa un numero entre el 1 y el 10");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
             }
+            while (!isGuessed);
+
+            Console.Read();
         }
     }
 }
