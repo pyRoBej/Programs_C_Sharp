9dffbab91e2f05ed99d19a69da5ca916147a7444
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-02 12:11:01 -0800

    creacion del programa try catch y envio del programa arrays 2 solo la asignacion 4

--- Arrays2/Arrays2.cs/Program.cs
+++ Arrays2/Arrays2.cs/Program.cs
@@ -12,88 +12,106 @@ namespace Arrays2.cs
         {
             //Ejercicio 2 de arrays 
             //asignacion 1
-            Console.WriteLine("The Tech Academy.\n Arrays.");
+            //Console.WriteLine("The Tech Academy.\n Arrays.");
 
-            string[] datos = { "Manuel", "Bejarano", "Lopez", "36", "años" };
+            //string[] datos = { "Manuel", "Bejarano", "Lopez", "36", "años" };
 
-            Console.WriteLine("Hola! ingresa el texto que quieras");
-            string text = Console.ReadLine();
+            //Console.WriteLine("Hola! ingresa el texto que quieras");
+            //string text = Console.ReadLine();
 
-            for (int i = 0; i < datos.Length; i++) //con .Length se ocupa para que el for se ejecute tantas veces se necesite mientras sea menor que la longitud del array datos
-            {
-                Console.Write(datos[i] + " " + text + "  ");
-            }
-            Console.ReadLine();
+            //for (int i = 0; i < datos.Length; i++) //con .Length se ocupa para que el for se ejecute tantas veces se necesite mientras sea menor que la longitud del array datos
+            //{
+            //    Console.Write(datos[i] + " " + text + "  ");
+            //}
+            //Console.ReadLine();
 
-            foreach (string dato in datos)
-            {
-                Console.WriteLine(dato);
-            }
-            Console.ReadLine();
+            //foreach (string dato in datos)
+            //{
+            //    Console.WriteLine(dato);
+            //}
+            //Console.ReadLine();
 
-            ////asignacion 2
-            //for (int i = 0; i <4; i++)
+            //////asignacion 2
+            ////for (int i = 0; i <4; i++)
+            ////{
+            ////    Console.WriteLine(datos[i]);
+            ////}
+            ////Console.ReadLine();
+
+            ////asignacion 3
+            //Console.Clear();
+            //for (int i = 0; i <4; i++) 
             //{
             //    Console.WriteLine(datos[i]);
             //}
             //Console.ReadLine();
 
-            //asignacion 3
-            Console.Clear();
-            for (int i = 0; i <4; i++) 
-            {
-                Console.WriteLine(datos[i]);
-            }
-            Console.ReadLine();
-
-            for (int i = 0; i <=4; i++) 
-            {
-                Console.WriteLine(datos[i]);
-            }
-            Console.ReadLine();
+            //for (int i = 0; i <=4; i++) 
+            //{
+            //    Console.WriteLine(datos[i]);
+            //}
+            //Console.ReadLine();
 
             //asignacion 4
             Console.Clear();
-            string datos2 = "Manuel Bejarano Lopez 36 años";
+            var datos2 = new[] { "Manuel", "Bejarano", "Lopez", "36", "años" }; //array implicito
             Console.WriteLine("ingresa un texto para buscar en la lista");
             string selecText = Console.ReadLine();
             Console.ReadLine();
-
-            Console.WriteLine("Se encontro el texto en el indice " + datos2.IndexOf(selecText));
-            Console.ReadLine();
-
-
-            //asignacion 5
-            Console.Clear();
-            //string[] datos3 = { "Manuel", "Bejarano", "Lopez", "36", "años" };
-
-            string datos3 = "Manuel Bejarano Lopez 36 años";
-            Console.WriteLine("ingresa un texto para buscar en la lista de cadenas");
-            string selecText2 = Console.ReadLine();
-            Console.ReadLine();
-            for (int i = 0; i < datos3.Length -1; i++)
+            for (int i = 0; i < datos2.Length; i++)
             {
-                int selecText3 = Convert.ToInt32(selecText2);
-                ////if (a > b)
-                if (datos3[i].CompareTo(datos3[selecText3]) > 0)
-                //    {
-                //    string selecText4 = Convert.ToString(selecText3);
-                //    Console.WriteLine("Se encontro el texto en el indice " + datos3.IndexOf(selecText4));
-                //    }
-                //}
-
-               
-
-                //if (datos3[i].CompareTo(datos3[selecText2]))
-                {
-                    Console.WriteLine("Se encontro el texto en el indice " + datos3.IndexOf(selecText2));
-                }
-                else
-                {
-                    Console.WriteLine("su entrada no esta en la lista");
-                }
+                if (datos2[i] == selecText)
+                    Console.WriteLine("Se encontro el texto en el indice " + datos2[i]);
             }
             Console.ReadLine();
+
+            //asignacion 4.2
+            //Console.Clear();
+            //var datos2 = new[] { "Manuel", "Bejarano", "Lopez", "36", "años" }; //array implicito
+            //List<string> strList = new List<string>();
+            //Console.WriteLine("ingresa un texto para buscar en la lista");
+            //string selecText = Console.ReadLine();
+            //Console.ReadLine();
+            //foreach (string dato in datos2)
+            //{
+            //    if (selecText = datos2[])
+            //    {
+            //        Console.WriteLine("el texto que ingresaste esta en el indice: " + datos2[] + "\n\n");
+            //    }
+            //    else
+            //        Console.WriteLine("el texto que ingresaste no existe en esta matriz\n\n");
+            //}
+            //Console.ReadLine();
+
+
+            ////asignacion 5
+            //Console.Clear();
+            ////string[] datos3 = { "Manuel", "Bejarano", "Lopez", "36", "años" };
+
+            //string datos3 = "Manuel Bejarano Lopez 36 años";
+            //Console.WriteLine("ingresa un texto para buscar en la lista de cadenas");
+            //string selecText2 = Console.ReadLine();
+            //Console.ReadLine();
+            //for (int i = 0; i < datos3.Length -1; i++)
+            //{
+            //    int selecText3 = Convert.ToInt32(selecText2);
+            //    ////if (a > b)
+            //    if (datos3[i].CompareTo(datos3[selecText3]) > 0)
+            //    //    {
+            //    //    string selecText4 = Convert.ToString(selecText3);
+            //    //    Console.WriteLine("Se encontro el texto en el indice " + datos3.IndexOf(selecText4));
+            //    //    }
+            //    //}
+            //    //if (datos3[i].CompareTo(datos3[selecText2]))
+            //    {
+            //        Console.WriteLine("Se encontro el texto en el indice " + datos3.IndexOf(selecText2));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("su entrada no esta en la lista");
+            //    }
+            //}
+            //Console.ReadLine();
 
         }
     }
---
+++ TryCatch/TryCatch.cs/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio manejo de excepciones
+            Console.WriteLine("The Tech Academy.\n Excepciones.");
+
+            try
+            {
+                int[] numeros = new int[5];
+
+                Console.WriteLine("Hola! ingrese un numero");
+                int numUno = Convert.ToInt32(Console.ReadLine());
+                numeros[0] = numUno;
+                Console.WriteLine("Ingrese un segundo numero");
+                int numDos = Convert.ToInt32(Console.ReadLine());
+                numeros[1] = numDos;
+                Console.WriteLine("Ingrese un tercer numero");
+                int numTres = Convert.ToInt32(Console.ReadLine());
+                numeros[2] = numTres;
+                Console.WriteLine("Ingrese un cuarto numero");
+                int numCuatro = Convert.ToInt32(Console.ReadLine());
+                numeros[3] = numCuatro;
+                Console.WriteLine("Ingrese un quinto numero");
+                int numCinco = Convert.ToInt32(Console.ReadLine());
+                numeros[4] = numCinco;
+                Console.WriteLine("Escoja un numero con el que se dividiran el resto de numeros");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+
+                for (int i =0; i < numeros.Length; i++)
+                {
+                    int result =  i / numberOne;
+                    Console.WriteLine("El 1er numero de la cadena dividido entre " + numberOne + " es gual a " + result);
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
