8a4a1b1a40f97da958fa3c98a1ab15e0698c7d14
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-02 16:41:22 -0800

    1a parte del try catch

--- TryCatch/TryCatch.cs/Program.cs
+++ TryCatch/TryCatch.cs/Program.cs
@@ -15,34 +15,34 @@ namespace TryCatch.cs
 
             try
             {
-                int[] numeros = new int[5];
+                List<int> numeros = new List<int>() { 20, 50, 60, 85 }; // lista de numeros enteros
 
-                Console.WriteLine("Hola! ingrese un numero");
-                int numUno = Convert.ToInt32(Console.ReadLine());
-                numeros[0] = numUno;
-                Console.WriteLine("Ingrese un segundo numero");
-                int numDos = Convert.ToInt32(Console.ReadLine());
-                numeros[1] = numDos;
-                Console.WriteLine("Ingrese un tercer numero");
-                int numTres = Convert.ToInt32(Console.ReadLine());
-                numeros[2] = numTres;
-                Console.WriteLine("Ingrese un cuarto numero");
-                int numCuatro = Convert.ToInt32(Console.ReadLine());
-                numeros[3] = numCuatro;
-                Console.WriteLine("Ingrese un quinto numero");
-                int numCinco = Convert.ToInt32(Console.ReadLine());
-                numeros[4] = numCinco;
-                Console.WriteLine("Escoja un numero con el que se dividiran el resto de numeros");
+                //Console.WriteLine("Hola! ingrese un numero");
+                //int numUno = Convert.ToInt32(Console.ReadLine());
+                //numeros[0] = numUno;
+                //Console.WriteLine("Ingrese un segundo numero");
+                //int numDos = Convert.ToInt32(Console.ReadLine());
+                //numeros[1] = numDos;
+                //Console.WriteLine("Ingrese un tercer numero");
+                //int numTres = Convert.ToInt32(Console.ReadLine());
+                //numeros[2] = numTres;
+                //Console.WriteLine("Ingrese un cuarto numero");
+                //int numCuatro = Convert.ToInt32(Console.ReadLine());
+                //numeros[3] = numCuatro;
+                //Console.WriteLine("Ingrese un quinto numero");
+                //int numCinco = Convert.ToInt32(Console.ReadLine());
+                //numeros[4] = numCinco;
+                Console.WriteLine("Escoja un numero entero con el que se dividiran el resto de numeros");
                 int numberOne = Convert.ToInt32(Console.ReadLine());
 
-                for (int i =0; i < numeros.Length; i++)
+                for (int i =0; i < numeros.Count; i++)
                 {
-                    int result =  i / numberOne;
-                    Console.WriteLine("El 1er numero de la cadena dividido entre " + numberOne + " es gual a " + result);
+                    int result =  numeros[i] / numberOne;
+                    Console.WriteLine("El numero en el indice " + i + " de la cadena dividido entre " + numberOne + " es gual a " + result);
                 }
                 Console.ReadLine();
             }
-            catch (FormatException ex)
+            catch (FormatException ex) 
             {
                 Console.WriteLine(ex.Message);
             }
