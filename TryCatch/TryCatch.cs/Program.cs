ec0266f3dbabdf855e9b5581d607c954779cd85d
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-03 11:04:31 -0800

    corrección de try catch

--- TryCatch/TryCatch.cs/Program.cs
+++ TryCatch/TryCatch.cs/Program.cs
@@ -17,36 +17,36 @@ namespace TryCatch.cs
             {
                 List<int> numeros = new List<int>() { 20, 50, 60, 85 }; // lista de numeros enteros
 
-                //Console.WriteLine("Hola! ingrese un numero");
-                //int numUno = Convert.ToInt32(Console.ReadLine());
-                //numeros[0] = numUno;
-                //Console.WriteLine("Ingrese un segundo numero");
-                //int numDos = Convert.ToInt32(Console.ReadLine());
-                //numeros[1] = numDos;
-                //Console.WriteLine("Ingrese un tercer numero");
-                //int numTres = Convert.ToInt32(Console.ReadLine());
-                //numeros[2] = numTres;
-                //Console.WriteLine("Ingrese un cuarto numero");
-                //int numCuatro = Convert.ToInt32(Console.ReadLine());
-                //numeros[3] = numCuatro;
-                //Console.WriteLine("Ingrese un quinto numero");
-                //int numCinco = Convert.ToInt32(Console.ReadLine());
-                //numeros[4] = numCinco;
                 Console.WriteLine("Escoja un numero entero con el que se dividiran el resto de numeros");
                 int numberOne = Convert.ToInt32(Console.ReadLine());
 
                 for (int i =0; i < numeros.Count; i++)
                 {
                     int result =  numeros[i] / numberOne;
-                    Console.WriteLine("El numero en el indice " + i + " de la cadena dividido entre " + numberOne + " es gual a " + result);
+                    Console.WriteLine("El numero " + " (" + numeros[i] + ") " + " que esta en el indice "+ i + " de la cadena, dividido entre " + numberOne + " es gual a " + result);
                 }
-                Console.ReadLine();
+                Console.Read();
             }
             catch (FormatException ex) 
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Por favor escriba un número entero");
+                return;
             }
-            Console.ReadLine();
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("No puede dividir entre cero, seleccione otro.");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            finally
+            {
+                Console.WriteLine("Ha salido del try/catch");
+                Console.ReadLine();
+            }
 
         }
     }
