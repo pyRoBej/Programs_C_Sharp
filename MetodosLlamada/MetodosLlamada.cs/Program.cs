a9a77b8da8819c922457d8a25d14114d4ee0d9f5
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-06 10:36:27 -0800

    se actualizo el programa metodo llamada para entregarlo: del proyecto herencia solo tome referencias

--- MetodosLlamada/MetodosLlamada.cs/Program.cs
+++ MetodosLlamada/MetodosLlamada.cs/Program.cs
@@ -8,8 +8,21 @@ namespace MetodosLlamada.cs
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //Ejercicio Metodos de llamada
+            
+            Console.WriteLine("Hola bienvenido, Selecciona un numero para hacer operaciones?");
+            int numero = Convert.ToInt32(Console.ReadLine()); //lee el valor
+
+            OpsMath ops= new OpsMath();
+
+            Console.WriteLine("El resultado de la operación suma es: " + ops.suma(numero)); //aqui le estoy diciendo que imprima el texto y le concatene la clase ops con su metodo suma
+            Console.WriteLine("El resultado de la operación resta es: " + ops.resta(numero));
+            Console.WriteLine("El resultado de la operación multiplicación es: " + ops.multp(numero));
+
+            Console.ReadLine();
+
         }
     }
 }
