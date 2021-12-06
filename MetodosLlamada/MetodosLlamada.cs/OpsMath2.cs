633a43d18bf833455661238528305a5bbd421057
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-06 12:04:48 -0800

    asgnacion 2 (279)

---
+++ MetodosLlamada/MetodosLlamada.cs/OpsMath2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosLlamada.cs
+{
+    class OpsMath2
+    {
+        public int suma(int num)    //el cambiar void por int significa que me va a regresar un valor, si dejo void le estaria diciendo
+                                    //que no devuelve nada, ademas para que no marque error debo cumplir este retorno, por eso
+                                    //dentro del metodo se pone el return result
+        {
+            int result = num + 10;
+            return result;
+            //Console.WriteLine("tu numero mas 10 es: " + result);
+        }
+
+        public int resta(decimal num)
+        {
+            int result = Convert.ToInt32(num) - 2;
+            return result;
+            //Console.WriteLine("tu numero menos 2 es: " + result);
+        }
+
+        public int multp(string num)
+        {
+            int result = Convert.ToInt32(num) * 10;
+            return result;
+            //Console.WriteLine("tu numero multiplicado por 10 es: " + result);
+        }
+    }
+}
--- MetodosLlamada/MetodosLlamada.cs/Program.cs
+++ MetodosLlamada/MetodosLlamada.cs/Program.cs
@@ -15,11 +15,25 @@ namespace MetodosLlamada.cs
             Console.WriteLine("Hola bienvenido, Selecciona un numero para hacer operaciones?");
             int numero = Convert.ToInt32(Console.ReadLine()); //lee el valor
 
-            OpsMath ops= new OpsMath();
+            //OpsMath ops= new OpsMath();
 
-            Console.WriteLine("El resultado de la operación suma es: " + ops.suma(numero)); //aqui le estoy diciendo que imprima el texto y le concatene la clase ops con su metodo suma
-            Console.WriteLine("El resultado de la operación resta es: " + ops.resta(numero));
-            Console.WriteLine("El resultado de la operación multiplicación es: " + ops.multp(numero));
+            //Console.WriteLine("El resultado de la operación suma es: " + ops.suma(numero)); //aqui le estoy diciendo que imprima el texto y le concatene la clase ops con su metodo suma
+            //Console.WriteLine("El resultado de la operación resta es: " + ops.resta(numero));
+            //Console.WriteLine("El resultado de la operación multiplicación es: " + ops.multp(numero));
+
+            //Console.ReadLine();
+
+            OpsMath2 ops = new OpsMath2();
+
+            Console.WriteLine("El resultado de la operación suma es: " + ops.suma(numero) + "\n"); //aqui le estoy diciendo que imprima el texto y le concatene la clase ops con su metodo suma
+            
+            Console.WriteLine("Escriba un numero con decimales para hacer operaciones?");
+            decimal numero2 = Convert.ToDecimal(Console.ReadLine()); //lee el valor
+            Console.WriteLine("El resultado de la operación resta es: " + ops.resta(numero2) + "\n");
+
+            Console.WriteLine("Escriba un numero para hacer operaciones?");
+            string numero3 = Console.ReadLine(); //lee el valor
+            Console.WriteLine("El resultado de la operación multiplicación es: " + ops.multp(numero3));
 
             Console.ReadLine();
 
