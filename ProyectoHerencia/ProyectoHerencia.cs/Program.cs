2e609c391c6763755d2c1a64da60d5f00e6d8145
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-06 00:59:55 -0800

    actualizacion del juego 21 hasta video 273, proyecto de aprendizaje de Herencia y creacion del programa de asignación

---
+++ ProyectoHerencia/ProyectoHerencia.cs/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHerencia.cs
+{
+    class Program //es la clase principal
+    {
+        static void Main(string[] args) //Main es donde comienza el flujo del programa, aqui se declaran los objetos/las instancias pertenecientes a nuestras clases
+        {
+            // las siguientes son las instancias
+            Caballo TiroAlBlanco = new Caballo();
+
+            Humano Manuel = new Humano();
+
+            Gorila Donkey = new Gorila();
+
+
+            //ahora vamos a usar las instancias pertenecientes a cada una de las clases anteriores (TiroAlBlanco, Manuel y Donkey), para utilizar sus metodos
+
+            TiroAlBlanco.galopar();
+
+
+        }
+    }
+
+            class Mamiferos //esta es la clase de mayor jerarquia, la que le hereda a los demas, (clase padre)
+            {
+                public void respirar()
+                {
+                    Console.WriteLine("Soy capaz de respirar");
+                }
+
+                public void cuidarCrias()
+                {
+                    Console.WriteLine("Cuido de mis crias hasta que se valen por si solas");
+                }
+            }
+
+                    class Caballo:Mamiferos //esta seria la clase hijo xq hereda los metodos de la clase padre (respirar y cuidar crias son los metodos);
+                                            //como sabemos que es clase hijo? xq dsps del nombre de la clase, la sintaxis lleva ":" y despues el nombre de la clase de la que hereda
+                    {
+                        public void galopar() //este metodo es exclusivo de la clase caballo, es un metodo que no hereda, es propio
+                        {
+                            Console.WriteLine("Soy capaz de galopar"); 
+                        }
+                    }
+
+                    class Humano:Mamiferos
+                    {
+                        public void pensar()
+                        {
+                            Console.WriteLine("Soy capaz de pensar");
+                        }
+                    }
+
+                    class Gorila:Mamiferos
+                    {
+                        public void trepar()
+                        {
+                            Console.WriteLine("Soy capaz de trepar");
+                        }
+                    }
+}
--- TwentyOne/TwentyOne.cs/Card.cs
+++ TwentyOne/TwentyOne.cs/Card.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TwentyOne.cs
 {
-    class Card
+    public class Card // para crear esta clase me fui al explorador de soluciones (en la barra del lado derecho oculto),
+                      // se abre se selecciona agregar/clase, y la de tipo C# (hasta arriba) es la que seleccione,
+                      // despues le di nombre y enter; las clases solo son un modelo, diseño para un objeto
     {
+        public Card()
+            {
+                Suit = "Spades";
+                Face = "Two";
+            }
+            public string Suit { get; set; } //aqui estoy declarando esta variable publica para que se pueda acceder desde otras partes del programa y asi.
+                                             //El get y set que van adentro es por que es lo unico que se puede hacer con ella
+            public string Face { get; set; } //esta otra variable publica es una propiedad mas de nuestra clase Card
+        
     }
 }
