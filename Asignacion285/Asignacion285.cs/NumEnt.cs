a86d87d6bbc9f8415edc9d76000b514d37193895
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-07 01:47:34 -0800

    creacion de la asignacion 285 y avances del juego 21

---
+++ Asignacion285/Asignacion285.cs/NumEnt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignacion285.cs
+{
+    class NumEnt
+    {
+        public void genNum(int numEnt)
+        {
+            //Random rnd = new Random();
+            //int numEnt = rnd.Next(1, 101);
+            int div = numEnt / 2;
+            //Console.WriteLine(numEnt);
+            Console.ReadLine();
+            Console.WriteLine("El numero entero dividido entre dos es igual = " + div);
+            Console.ReadKey();
+        }
+
+        public uint genNum(uint num)
+        {
+            uint div = num / 2;
+            return div;
+            Console.WriteLine(div);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Asignacion285/Asignacion285.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignacion285.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio Metodos de llamada
+
+            Console.WriteLine("Hola, a continuacion haremos una división, proporcione el número al que vamos a dividir entre 2");
+            int num1 = Convert.ToInt32(Console.ReadLine()); //lee el valor
+
+            NumEnt num = new NumEnt();  //es la instancia que llama a mi clase NumEnt y al metodo genNum
+
+            num.genNum(num1);               // aqui estoy haciendo llamar al metodo genNum que esta en la clase NumEnt
+
+            Console.ReadKey();
+        }
+    }
+}
