3f1f7f1c3f2ca614af8554e8a511ead14783b1a7
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-06 16:52:58 -0800

    correccion de asignacion 279 y primera parte de asignacion 281

---
+++ Asignacion281/Asignacion281.cs/Ops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignacion281.cs
+{
+    class Ops
+    {
+        public void multp(int num1, int num2)    //el void significa que no me va a regresar un valor, le estoy diciendo
+                                                           //que no devuelve nada
+        {
+            int result = num1 * 10;
+            Console.WriteLine(result + "\n");
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ Asignacion281/Asignacion281.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignacion281.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio Metodos de llamada
+
+            Console.WriteLine("Hola, a continuacion haremos una multiplicación, proporcione el primer número");
+            int num1 = Convert.ToInt32(Console.ReadLine()); //lee el valor
+            Console.WriteLine("...proporcione el segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine()); //lee el valor
+
+            Ops ops = new Ops();
+
+            //Console.WriteLine("El resultado de la multiplicación es: "  ops.multp(num1, num2) "\n");
+            Console.WriteLine(ops.multp(num1, num2);
+            Console.ReadLine();
+
+        }
+
+    }
+}
