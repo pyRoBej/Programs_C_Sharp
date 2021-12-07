99d1f04f76bf9ccbd29651a6826af12df3391f2a
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-07 03:59:30 -0800

    avance del juego 21 hasta el paso 289

--- TwentyOne/TwentyOne.cs/Program.cs
+++ TwentyOne/TwentyOne.cs/Program.cs
@@ -10,46 +10,28 @@ namespace TwentyOne.cs
     {
         static void Main(string[] args)
         {
+            //    Game game = new Game();
+            //    game.Dealer = "Manuel";
+            //    game.Name = "21";
+            //Card card = new Card() { Face = "King", Suit = "Spades" };   //inicializacion de objeto
+            
+            
+            //Game game = new Game();
+            //game.Players = new List<string>() { "pyRo", "David", "Mayra" };
+            //game.ListPlayer();
+            //game.Play();
+            Console.ReadLine();
+
+
             Deck deck = new Deck();
-            int timesShuffled = 0;
-            deck = Shuffle(deck, out timesShuffled, 3);
+            deck.Shuffle(3);
       
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("Times Shuffled : {0}", timesShuffled);
             Console.ReadLine();
         }
-
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++)
-            {
-                timesShuffled++;
-                List<Card> TempList = new List<Card>();
-                Random random = new Random();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    TempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-                }
-                deck.Cards = TempList;
-            }
-            return deck;
-        }
-
-        //public static Deck Shuffle(Deck deck, int times = 1)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
---
+++ TwentyOne/TwentyOne.cs/TwentyOneGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne.cs
+{
+    public class TwentyOneGame : Game   //para heredar simplemente hay que poner ":" (dos puntos) y la clase de la que heredara
+    {
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
