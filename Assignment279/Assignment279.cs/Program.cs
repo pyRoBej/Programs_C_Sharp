fd19ca6a6ffff5eeca17b22caf12acae452489c2
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-07 00:56:28 -0800

    actualización  de asignación 279

--- Assignment279/Assignment279.cs/Program.cs
+++ Assignment279/Assignment279.cs/Program.cs
@@ -17,15 +17,15 @@ namespace Assignment279.cs
 
             OpsMath2 ops = new OpsMath2();
 
-            Console.WriteLine("El resultado de la operación suma es: " + ops.suma(numero) + "\n"); //aqui le estoy diciendo que imprima el texto y le concatene la clase ops con su metodo suma
+            Console.WriteLine("El resultado de la operación suma es: " + ops.suma(numero) + "\n");
 
             Console.WriteLine("Escriba un numero con decimales para hacer operaciones?");
             decimal numero2 = Convert.ToDecimal(Console.ReadLine()); //lee el valor
-            Console.WriteLine("El resultado de la operación resta es: " + ops.resta(numero2) + "\n");
+            Console.WriteLine("El resultado de la operación resta es: " + ops.suma(numero2) + "\n");
 
             Console.WriteLine("Escriba un numero para hacer operaciones?");
             string numero3 = Console.ReadLine(); //lee el valor
-            Console.WriteLine("El resultado de la operación multiplicación es: " + ops.multp(numero3));
+            Console.WriteLine("El resultado de la operación multiplicación es: " + ops.suma(numero3)); //sobre carga del metodo
 
             Console.ReadLine();
         }
