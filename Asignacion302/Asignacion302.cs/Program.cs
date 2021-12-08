9834e1718f2a93fcd706b8f47573f38647f36ce4
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-08 12:51:50 -0800

    comentarios a 302

--- Asignacion302/Asignacion302.cs/Program.cs
+++ Asignacion302/Asignacion302.cs/Program.cs
@@ -11,13 +11,15 @@ namespace Asignacion302.cs
         static void Main(string[] args)
         {
 
-            //Employee empleado1 = new Employee();
+            Employee empleado1 = new Employee();
             IQuitTable empleado = new Employee();
 
+            
+            empleado1.firstName = "Assigment";
+            empleado1.lastName = "305";
+            empleado1.sayName();             //estoy mandando llamar el metodo dentro de la clase employee que me imprime un nombre completo con los valores que meti anteriormente a firstName y lastName
+
             empleado.Quit();
-            //empleado.firstName = "Assigment";
-            //empleado.lastName = "305";
-            //empleado.sayName();             //estoy mandando llamar el metodo dentro de la clase employee que me imprime un nombre completo con los valores que meti anteriormente a firstName y lastName
 
             Console.ReadKey();
 
