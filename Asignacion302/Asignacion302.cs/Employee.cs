eb9c59b4c8311ae13dcad0bf981e0943d184ef25
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-07 17:21:54 -0800

    avance de la asignación 302

---
+++ Asignacion302/Asignacion302.cs/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignacion302.cs
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+
+        Person.
+
+
+        
+        
+    }
+    public void sayName()
+    {
+        Console.WriteLine($"Nombre es: [ {FirstName} {LastName}]");
+    }
+}
