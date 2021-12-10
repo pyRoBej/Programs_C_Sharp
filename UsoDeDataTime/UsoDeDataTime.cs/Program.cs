6240329bf891d4901703a6c8db58165ca7862f43
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-10 11:55:28 -0800

    asignación 369

--- UsoDeDataTime/UsoDeDataTime.cs/Program.cs
+++ UsoDeDataTime/UsoDeDataTime.cs/Program.cs
@@ -13,14 +13,17 @@ namespace UsoDeDataTime.cs
             Console.WriteLine("Fecha y hora actual");
             Console.WriteLine(DateTime.Now.DayOfWeek);
             Console.WriteLine(DateTime.Now);
-            Console.ReadLine();
+            //Console.ReadLine();
 
-            Console.WriteLine("Introduce un numero para sumar en horas a la fecha actual");
+            Console.WriteLine("Enter a number to add the hours to the current date");
+            //Console.WriteLine("Introduce un numero para sumar las horas a la fecha actual");
             int num = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
 
-
-
+            //Console.WriteLine(DateTime.Now.AddHours(num));
+            //Console.WriteLine("La fecha de hoy es: " + DateTime.Now + num + "horas.");
+            Console.WriteLine("Today's date plus " + num + " hours "  + " is: " + DateTime.Now.AddHours(num) );
+            Console.ReadLine();
         }
     }
 }
