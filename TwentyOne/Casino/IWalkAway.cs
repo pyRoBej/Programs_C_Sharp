774283666831db6bbd724c3cc134e9f91afa3ffe
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-10 15:58:30 -0800

    cambios en las clases para poder usar la librería Casino

--- TwentyOne/Casino/IWalkAway.cs
+++ TwentyOne/Casino/IWalkAway.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Casino
+namespace Casino.Interfaces
 {
     interface IWalkAway   //una interfaz es similar a una clase abstracta
     {
--- TwentyOne/Casino/TwentyOneGame.cs
+++ TwentyOne/Casino/TwentyOneGame.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Casino.Interfaces;
 
-namespace Casino
+namespace Casino.TwentyOne
 {
     public class TwentyOneGame : Game, IWalkAway   //para heredar simplemente hay que poner ":" (dos puntos) y la clase de la que heredara;
                                                    //la segunda que se hereda no es una clase, es una interfaz, es por eso que es posible heredarla
