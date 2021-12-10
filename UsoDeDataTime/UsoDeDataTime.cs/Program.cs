471a7710bcbc7e74f5bb8cc6b541e358cf0c8fba
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-10 11:21:59 -0800

    aprendiendo a usar Date Time

---
+++ UsoDeDataTime/UsoDeDataTime.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoDeDataTime.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fecha y hora actual");
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+
+            Console.WriteLine("Introduce un numero para sumar en horas a la fecha actual");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
