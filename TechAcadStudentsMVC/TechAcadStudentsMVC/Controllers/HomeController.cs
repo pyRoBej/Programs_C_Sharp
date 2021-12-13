9bd4d2e75925f6186e63b2fdeaa45669dc4931c8
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-12-13 14:56:03 -0800

    asignación de once partes de MVC

--- TechAcadStudentsMVC/TechAcadStudentsMVC/Controllers/HomeController.cs
+++ TechAcadStudentsMVC/TechAcadStudentsMVC/Controllers/HomeController.cs
@@ -6,24 +6,38 @@ using System.Web.Mvc;
 
 namespace TechAcadStudentsMVC.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller  //hereda de la clase Controller, en la que estan definidos los metodos View()
+                                              //y el pseudo-diccionario llamado ViewBag(), si le quitamos esta clase no reconocera los metodos
     {
-        public ActionResult Index()
+        public ActionResult Index()     //este metodo (Index) no acepta parametros y su tipo de retorno es un tipo de datos llamado ActionResult
         {
-            return View();
+            return View();              //ActionResult es una clase definida en la biblioteca System.Web.Mvc (es como HTML renderizado)
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View();              //el metodo View es un metodo muy poderoso, observando el nombre del metodo del que se llama
+                                        //(Index/About/Contact) y el nombre de la clase controlador (HomeController) puede encontrar
+                                        //y procesar el archivo .cshtml apropiado (Index.cshtml).
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact page - The Tech Academy.";
 
+            return View();
+        }
+
+        public ActionResult Instructor(int id=7718094)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public ActionResult Instructors()
+        {
             return View();
         }
     }
